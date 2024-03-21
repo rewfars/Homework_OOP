18c9c27ecb7e90e614ea5151c19874235650bc77
rewfars <nazarnurlanov6@gmail.com>
2024-03-21 17:07:21 +0600

    hw6

---
+++ nazar_nurlanov_40-1_hw6.py
@@ -0,0 +1,45 @@
+def bubble_sort(bubble):
+    n = len(bubble)
+    while n > 0:
+        n -= 1
+        for i in range (0, n):
+            if bubble[i] > bubble[i+1]:
+                bubble[i] , bubble[i + 1]= bubble[i+1], bubble[i]
+numbers = [24, 53, 4, 35, 18, 32, 2, 74, 28]
+bubble_sort(numbers)
+print(numbers)
+
+
+
+import random
+n = 5000
+resultOK = False
+first = 0
+last = n - 1
+pos = 0
+def binary_search(val):
+    global n, resultOK, first, last, pos
+
+    while first < last:
+        middle = (first + last)//2
+        if val==middle:
+            first = middle
+            last = first
+            resultOK = True
+            pos = middle
+        else :
+            if val > middle:
+                first = middle+1
+            else :
+                last = middle-1
+
+
+    if resultOK:
+        print('элемент найден')
+        print(pos)
+    else:
+        print('элемент не найден')
+
+val = random.randint(first, n)
+print(val)
+binary_search(val)
